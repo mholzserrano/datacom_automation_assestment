21f0d0b5ca1057f3ef1313f7085f6a8ab43ed3ea
mserrano <mholie@ubx.ph>
2022-08-31 23:42:33 +0800

    Updated the xpath for the tab account locator.

--- RESOURCES/LOCATORS/COMPONENTS/component_pay_transfer_modal.py
+++ RESOURCES/LOCATORS/COMPONENTS/component_pay_transfer_modal.py
@@ -3,7 +3,7 @@ COMPONENT_PAY_TRANSFER_MODAL={
     "BUTTON_TO_ACCOUNT":        "xpath://button[@data-monitoring-label='Transfer Form To Chooser']",
     "INPUT_AMOUNT":             "xpath://input[@data-monitoring-label='Transfer Form Amount']",
     "BUTTON_EVERYDAY":          "xpath://p[text()='Everyday']/ancestor::button",
-    "TAB_ACCOUNT":              "xpath://li[@data-testid='to-account-accounts-tab']",
+    "TAB_ACCOUNT":              "xpath://span[text()='Accounts']/ancestor::li",
     "BUTTON_BILLS":             "xpath://p[text()='Bills ']/ancestor::button",
     "BUTTON_TRANSFER":          "xpath://button[@data-monitoring-label='Transfer Form Submit']"
 }
